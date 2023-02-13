4077d7a6f777bbc803df3839315f8a832ae467a1
Prem Singh <personalaarav@gmail.com>
2023-02-13 20:58:48 +0530

    starting javascript

---
+++ JavaScript/13 FEB Starting JavaScript/Values.js
@@ -0,0 +1,68 @@
+// Starting JavaScript 
+
+// VALUES - 
+
+// 7 => int
+// 9.1 => Float
+// But in JavaScript everything is treated as Numbers
+
+// 'Anurag'
+// "Anurag"
+// These are Strings
+
+// true or false
+// Boolean values
+
+// Null 
+// Undefined 
+// Sir will teach it later
+
+// Primitive Datatype
+// Single value are called Primitive Datatype
+
+// Non - Primitive values => Multi values
+// arrays => [] => index
+// objects => {} => Key:Value
+
+// () => Parenthisis, {} => Curely Brackets, [] => Square Brackets
+
+// ["A", "B", "C", "D", "E", "F", "G"]
+
+// {
+//     "key": "value"
+// }
+
+// -------------------------------------------Variables----------------------------------------------------------
+
+// var (we don't use this anymore), let, const
+
+// const => We need to give the value when we are declaring the variable
+
+let number = 2510;
+
+let number2 = 34;
+
+console.log(number);
+
+console.log(number2);
+
+// ------------------------------------------Operations------------------------------------------------------------
+
+let a = 50; 
+let b = 25;
+let c = 50;
+
+console.log("sum of a and b is - ", a+b);
+
+// Arithmetic Operator
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+console.log(3 ** 3);
+
+// Comparision OP
+console.log(a == b);
