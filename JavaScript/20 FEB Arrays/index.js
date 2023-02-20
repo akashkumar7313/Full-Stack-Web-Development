8fd5a59d8d267001d744ae8bd7b25714b6253a74
Prem Singh <personalaarav@gmail.com>
2023-02-20 22:21:15 +0530

    started arrays

---
+++ JavaScript/20 FEB Arrays/index.js
@@ -0,0 +1,27 @@
+// array => Seqencial linear data stucture
+
+let arr = [1,2,3,4,5];
+
+console.log(arr);
+
+console.log(arr[3]);
+
+let x = ["Prem", 1, 2, 3, "Delhi", false, 2.3]; 
+
+console.log(x);
+
+let a = new Array(10);
+
+console.log(a);
+
+// Second way of writting array 
+let colors = Array("black", "blue", "red", "white");
+console.log(colors); 
+
+// How to update data in array 
+let y = [22,19,30,1,6]; 
+
+y[1] = 99;
+
+console.log(y);
+
