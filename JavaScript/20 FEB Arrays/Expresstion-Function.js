6c27a3ca2fda1ab030745b5e0006ca2af414930a
Prem Singh <personalaarav@gmail.com>
2023-02-24 16:19:12 +0530

    expression functions

---
+++ JavaScript/20 FEB Arrays/Expresstion-Function.js
@@ -0,0 +1,24 @@
+// Function expression 
+
+let variableName = function functionName() {
+    // fucntion body
+}
+
+console.log(variableName());
+
+function add(x,y) {
+    return x + y;
+}
+
+let addTwoNumbers = function (x,y) {
+    return x + y;
+
+}
+
+let addTwoNumbersAgain = function eval(x,y) {
+    return x + y;
+}
+
+console.log(add(10, 2));
+console.log(addTwoNumbers(9,8));
+console.log(addTwoNumbersAgain(1,2))
