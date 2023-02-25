ec6c61a770235dc2af91cdcdbcde84b9f13b3169
Prem Singh <personalaarav@gmail.com>
2023-02-25 08:45:15 +0530

    done with objects

--- JavaScript/20 FEB Objects/objects.js
+++ JavaScript/20 FEB Objects/objects.js
@@ -7,3 +7,40 @@ let obj = {
 
 console.log(obj);
 console.log(obj.id + " " + obj.name + " " + obj.salary);
+/////////////////////////////////////////////////////////////////////////
+
+let emp = new Object  ();
+emp.id = 102;
+emp.name = "Sam";
+emp.salary = 12000;
+console.log(emp);
+
+// Creating object using function 
+function Emp (id, name, salary) {
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+}
+
+const e = new Emp(101, "Alex",10000);
+console.log(e);
+///////////////////////////////////////////////////////////////////////////////
+
+// Manipulating objects
+const empty = {
+    id: 101,
+    name: "Rahul",
+    salary: 15000,
+};
+
+console.log(empty.id);
+console.log(empty["id"]);
+empty.items = 10;
+empty['type'] = 'intern';
+
+empty.id = 12;
+empty['salary'] = 11000;
+console.log(empty);
+
+delete empty.name;  // Using delete keyword we can delete key value pairs.
+console.log(empty);
