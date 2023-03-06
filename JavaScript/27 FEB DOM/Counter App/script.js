7204bbf99913c49a1df8761841af311ac47ec8a4
Prem Singh <personalaarav@gmail.com>
2023-03-06 14:01:58 +0530

    javascript done of counter app

---
+++ JavaScript/27 FEB DOM/Counter App/script.js
@@ -0,0 +1,31 @@
+// getting the html elements
+const decrementBtn = document.getElementById("decrementBtn");
+const incrementBtn = document.getElementById("incrementBtn");
+const resetBtn = document.getElementById("resetBtn");
+const displayValue = document.getElementById("displayValue");
+
+// function for increment button 
+incrementBtn.addEventListener("click" , () => {
+    const value = Number(displayValue.innerText);
+    if(value >= 10){
+        alert("10+ values are not allowed");
+    } else {
+        displayValue.innerText = value + 1;
+    }
+});
+
+// function for decrement button 
+decrementBtn.addEventListener("click", () => {
+    const value = Number(displayValue.innerText);
+    if(value > 0) {
+        displayValue.innerText = value - 1;
+    } else {
+        alert("Negetive value not allowed");
+    }
+});
+
+
+// reset button
+resetBtn.addEventListener("click" , () => {
+    displayValue.innerText=0;
+});
