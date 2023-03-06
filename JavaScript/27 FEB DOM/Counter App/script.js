ca4cebc460d341302b56c2a44df3cefaa98f6dae
Prem Singh <personalaarav@gmail.com>
2023-03-06 14:05:57 +0530

    some changes

--- JavaScript/27 FEB DOM/Counter App/script.js
+++ JavaScript/27 FEB DOM/Counter App/script.js
@@ -4,16 +4,6 @@ const incrementBtn = document.getElementById("incrementBtn");
 const resetBtn = document.getElementById("resetBtn");
 const displayValue = document.getElementById("displayValue");
 
-// function for increment button 
-incrementBtn.addEventListener("click" , () => {
-    const value = Number(displayValue.innerText);
-    if(value >= 10){
-        alert("10+ values are not allowed");
-    } else {
-        displayValue.innerText = value + 1;
-    }
-});
-
 // function for decrement button 
 decrementBtn.addEventListener("click", () => {
     const value = Number(displayValue.innerText);
@@ -24,8 +14,18 @@ decrementBtn.addEventListener("click", () => {
     }
 });
 
+// function for increment button 
+incrementBtn.addEventListener("click" , () => {
+    const value = Number(displayValue.innerText);
+    if(value >= 10){
+        alert("10+ values are not allowed");
+    } else {
+        displayValue.innerText = value + 1;
+    }
+});
+
 
 // reset button
-resetBtn.addEventListener("click" , () => {
-    displayValue.innerText=0;
+resetBtn.addEventListener("click", () => {
+    displayValue.innerText = 0;
 });
