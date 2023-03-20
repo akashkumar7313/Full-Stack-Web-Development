9bade3c860376b48401347c78e0f438b96b4a2f5
Prem Singh <personalaarav@gmail.com>
2023-03-20 19:20:34 +0530

    Done

--- JavaScript/March/20 MAR LIVE/script.js
+++ JavaScript/March/20 MAR LIVE/script.js
@@ -8,4 +8,39 @@ console.log(x, y);
 
 // objects => {key: value}, {x:10, y:20} 
 
-// Operators 
+
+// Operators ==>
+
+// Arithmetic Operators
+console.log(10 + 2);
+console.log(10 - 2);
+console.log(10 * 2);
+console.log(10 / 2);
+console.log(10 % 2);
+console.log(10 ** 2);
+
+// Assignment Operators
+let a = 10; // = operator assigns value from rhs to lhs
+a += 9; // a = a + 9; a = 19;
+console.log(a);
+a -= 9; // a = a - 9;
+console.log(a);
+a *= 2;
+console.log(a);
+a /= 5;
+console.log(a);
+a **= 2;
+console.log(a);
+a %= 5;
+console.log(a);
+
+// Comparision Operator
+let m = 9;
+let n = 5;
+console.log(m>n); 
+console.log(m>=n); 
+console.log(m<n); 
+console.log(m<=n); 
+// ==, ===
+
+// Logical Operators 
