a4ef58fc9074376add9bad94b0267a153ea68396
Prem Singh <personalaarav@gmail.com>
2023-03-20 18:37:36 +0530

    starting revision of operators

--- JavaScript/March/20 MAR LIVE/script.js
+++ JavaScript/March/20 MAR LIVE/script.js
@@ -1,4 +1,11 @@
-x = 10;
-console.log(x);
+// Live Class Date => 20 March 23
+
+let x = 10;             // we can use let, var, const
+let y = "Aarav";
+let b = true;
+
+console.log(x, y);
 
 // objects => {key: value}, {x:10, y:20} 
+
+// Operators 
